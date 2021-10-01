5a94725655bbac9350d1403fd02ff87a173ef691
CLB Diễn thuyết và Tranh biện S&D <91719994+sad-ntu@users.noreply.github.com>
2021-10-01 22:30:20 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,8 +13,7 @@ $(document).ready(function () {
     var preview = $('.preview-image');
     var scaleX = 0.5;
     var scaleY = 0.5;
-    var output_size = 1024; // px
-    var output_name = rand_string(5)+'YTC-avatar.png';
+    var output_size = 1024; 
     
         var text = { 'frames' : [
 		{'name':'Khung demo CLB S&D', 'src' :'uploads/1234.png'}, 
